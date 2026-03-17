a04916f6fab7d5943dec5d4fdc6d1cab52f9dec2
Marco <228495880+donmarco3@users.noreply.github.com>
2026-03-17 21:30:49 +1030

    Delete src/keys.js

--- src/keys.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDU_N-NF6XmwojAvq6xQuk8_0rOEJi9t9I",
-  authDomain: "book-brain-ed740.firebaseapp.com",
-  projectId: "book-brain-ed740",
-  storageBucket: "book-brain-ed740.firebasestorage.app",
-  messagingSenderId: "283645225322",
-  appId: "1:283645225322:web:ac20b04396d3695c375d13"
-};
