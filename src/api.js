32bec82debdc37ab6d62512890360e5ace726006
Marco <228495880+donmarco3@users.noreply.github.com>
2026-03-17 21:24:19 +1030

    moved api key

--- src/api.js
+++ src/api.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, updateDoc, getDoc, query, where, serverTimestamp } from "firebase/firestore"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDU_N-NF6XmwojAvq6xQuk8_0rOEJi9t9I",
-  authDomain: "book-brain-ed740.firebaseapp.com",
-  projectId: "book-brain-ed740",
-  storageBucket: "book-brain-ed740.firebasestorage.app",
-  messagingSenderId: "283645225322",
-  appId: "1:283645225322:web:ac20b04396d3695c375d13"
-};
+import { firebaseConfig } from "./keys";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
