0fe65062443e07eae9d23558cecf33a2e9df3b58
Marco <228495880+donmarco3@users.noreply.github.com>
2026-04-24 11:52:00 +0930

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // String shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      '/api': 'http://localhost:3000',
+    },
+  },
 })
